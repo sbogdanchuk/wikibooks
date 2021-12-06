a46523878a07e6024d4878cd3efad5f6e106634f
siarhei.bahdanchuk <siarhei.bahdanchuk@verint.com>
2021-12-06 13:33:40 +0200

    feat: Add slick dbread; fix logging

--- build.sbt
+++ build.sbt
@@ -2,13 +2,29 @@ name := "wikibooks"
 
 version := "0.1"
 
+addDependencyTreePlugin
+
 scalaVersion := "2.12.10"
 val AkkaVersion = "2.6.17"
+val AkkaHttpVersion = "10.2.7"
 
 idePackagePrefix := Some("org.wikibooks")
 
+//excludeDependencies += "commons-logging" % "commons-logging"
+excludeDependencies ++= Seq(
+  ExclusionRule(organization="org.apache.logging.log4j", name="log4j-slf4j-impl")
+)
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+  "org.apache.spark" %% "spark-core" % "3.1.2" exclude("org.slf4j", "*"),
+  "com.typesafe.slick" %% "slick" % "3.3.3",
+  "com.typesafe.slick" %% "slick-hikaricp" % "3.3.3",
+//  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "org.slf4j" % "slf4j-api" % "1.6.6",
+  "org.slf4j" % "slf4j-log4j12" % "1.6.6",
+  "org.xerial" % "sqlite-jdbc" % "3.7.2"
 //  "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
-  "org.apache.spark" %% "spark-core" % "3.1.2"
 )
---
+++ src/main/scala/meetups/meet02/LoadData.scala
@@ -0,0 +1,69 @@
+package org.wikibooks
+package meetups.meet02
+
+import org.slf4j.LoggerFactory
+import slick.jdbc.SQLiteProfile.api._
+import slick.jdbc.JdbcBackend.Database
+
+import java.sql.SQLException
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
+
+/**
+ *
+ * @author Siarhei Bahdanchuk
+ *         Date: 25.11.2021
+ *
+ */
+
+/**
+ * CREATE TABLE "en"(
+ * "title" TEXT,
+ * "url" TEXT,
+ * "abstract" TEXT,
+ * "body_text" TEXT,
+ * "body_html" TEXT
+ * )
+ */
+
+case class Book(title: String, url: String, abstrct: String, bodyText: String, bodyHtml: String)
+
+class Books(tag: Tag) extends Table[Book](tag, "en") {
+  def title = column[String]("title")
+
+  def url = column[String]("url")
+
+  def abstrct = column[String]("abstract")
+
+  def body_text = column[String]("body_text")
+
+  def body_html = column[String]("body_html")
+
+  override def * = (title, url, abstrct, body_text, body_html) <> (Book.tupled, Book.unapply)
+}
+
+object LoadData extends App {
+  val log = LoggerFactory.getLogger("LoadData")
+  val books = TableQuery[Books]
+  val db = Database.forConfig("books")
+
+  val all = books.result
+  val filteredQuery = books.filter(_.title startsWith ("Wiki"))
+
+  try {
+    log.info("Get something from en books")
+    val allFuture = db.run[Seq[Book]](filteredQuery.result)
+    allFuture.onComplete {
+      case Success(s) => {
+        log.info(s"Got ${s.size} books")
+      }
+      case Failure(cause) => {
+        log.info(s"An error has occurred $cause")
+      }
+    }
+    Await.ready(allFuture, Duration.Inf)
+    log.info(s"end ${allFuture.isCompleted}")
+  } finally db.close
+}
